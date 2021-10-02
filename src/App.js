f9e197b83a914e754aa2857b01a3d32d44bd205f
Kally <60466044+kallyas@users.noreply.github.com>
2021-10-02 10:26:30 +0000

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import './App.css';
-import Home from "./pages/Home"
-import Nav from "./components/Nav"
-import Footer from "./components/Footer"
-import Sidebar from "./components/Sidebar"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+
+import Data from "./Library/stock";
 
 const items = [
-  { name: 'Dresses', label: 'Dresses' },
-  { name: 'Formal', label: 'Formal' },
-  { name: 'Jeans', label: 'Jeans' },
-  { name: 'Skirts', label: 'Skirts' },
-  { name: 'Tops', label: 'Tops' },
-]
+  { name: "Dresses", label: "Dresses" },
+  { name: "Formal", label: "Formal" },
+  { name: "Jeans", label: "Jeans" },
+  { name: "Skirts", label: "Skirts" },
+  { name: "Tops", label: "Tops" },
+];
 
 function App() {
+  const [data, setData] = useState(Data);
+  const [checked, setChecked] = useState(false)
+
+  const filterData = (data, category) => {
+    const filtered = data.filter((dt) => data.category === category);
+    setData(filtered);
+  };
+
+  const filter = () => {
+    setChecked(!checked)
+  }
+
   return (
     <div className="App">
-      <Nav/>
-      <Home/>
-      <Sidebar items={items}/>
-      <Footer/>
+      <div className="main-banner inner" id="home">
+        <Nav />
+      </div>
+      <main>
+        <section className="ab-info-main py-md-5 py-4">
+          <div className="container py-md-3">
+            <Home data={data} onFilter={filter} checked={checked} />
+          </div>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
