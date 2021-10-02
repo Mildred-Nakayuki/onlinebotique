af6ea5345af3ab8c0b6c05227dd423b624929a54
Kally <60466044+kallyas@users.noreply.github.com>
2021-10-02 10:25:40 +0000

    restructurehome page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,25 @@
+import Card from "../components/Card";
+import Sidebar from "../components/Sidebar";
 
-import {useState,useEffect} from 'react'
-import Card from '../components/Card'
-import Data from "../Library/stock"
-
-
-function Home() {
-const [data,setData]= useState(Data)
-  
-    return (
-        <div className="Home">
-           {
-             data.map(item =>(
-               <>
-<div className="titles">{item.category}</div>
-           <div className="body">
-    <Card data={item}/>
+function Home({ data, onFilter, checked }) {
+  return (
+    <div className="row">
+      <Sidebar onFilter={onFilter} checked={checked}/>
+      <div className="left-ads-display col-lg-8">
+        <div className="row">
+          {data.map((item) => (
+            <Card key={item.id} data={item} />
+          ))}
         </div>
-        </>
-             ))
-             
-             }
-           </div>         
-    )
+        <div className="grid-img-right mt-4 text-right">
+          <span className="money">Flat 50% Off</span>
+          <a href="/" className="btn">
+            Shop Now
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
